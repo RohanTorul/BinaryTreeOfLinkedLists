bf7900654e2bd1035f182a528658fb940a5c691d
Rohan <rohantorul@gmail.com>
2023-07-02 04:27:41 -0230

    added Node and Binary tree Struct

--- src/mainHeader.h
+++ src/mainHeader.h
@@ -1,1 +1,17 @@
 #pragma once
+
+typedef struct Nod
+{
+    char DataType[24];
+    void* Data_P;
+};
+
+
+typedef struct  BT
+{
+    char DataType[24];
+    void* ChildL;
+    void* ChildR;
+
+} BinaryTree;
+
