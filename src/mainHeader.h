1724c9126b55045dd0eabd6be4ff154df7f99886
Rohan <rohantorul@gmail.com>
2023-07-03 12:12:52 -0230

    added String type, and started Implementation

--- src/mainHeader.h
+++ src/mainHeader.h
@@ -4,52 +4,53 @@ int E;
 typedef struct string
 {
     int length;
-    char value[]; 
-}String;
+    char value[];
+} String;
 
 typedef struct Nod
 {
-    char DataType[24];
-    void* Data_P;
+    String Datatype;
+    void *Data_P;
 } Node;
 
-typedef struct  BT
-{   
+typedef struct BT
+{
     Node Leaf;
-    BinaryTree* Lbranch;
-    BinaryTree* Rbranch;
-    BinaryTree* Parent;
+    BinaryTree *Lbranch;
+    BinaryTree *Rbranch;
+    BinaryTree *Parent;
 } BinaryTree;
- 
+
 typedef struct Ll
-{   
-    LinkedList* Head;
-    LinkedList* Tail;
+{
+    LinkedList *Head;
+    LinkedList *Tail;
     Node node;
-    LinkedList* NextNode;
-    LinkedList* PreviousNode;
+    LinkedList *NextNode;
+    LinkedList *PreviousNode;
 
 } LinkedList;
 
-int newBT(BinaryTree* bt ,Node Root);//used to Initialise a tree, returns 0 if success
+int copytostring(String *s, String copyvalue); // copies the value of one string to another string
 
-int newLl(LinkedList* ln, Node Head);//used to initialise a Linked List, returns 0 if success
+int newBT(BinaryTree *bt, Node Root); // used to Initialise a tree, returns 0 if success
 
-int addLeaf(Node Leaf, Node* Parent);//Add a leaf to a tree, given it's parents, returns 0 if success
+int newLl(LinkedList *ln, Node Head); // used to initialise a Linked List, returns 0 if success
 
-int queueList(Node node);//Appends a node to the linked list (like in a queue structure), returns 0 if success
+int addLeaf(Node Leaf, Node *Parent); // Add a leaf to a tree, given it's parents, returns 0 if success
 
-int dequeueList(Node node);//removes the head of the linked list (Like in a queue structure), returns 0 if success
+int queueList(Node node); // Appends a node to the linked list (like in a queue structure), returns 0 if success
 
-int pushList(Node node);//Inserts a node as a new head of the Linked list (Like a stack structure), returns 0 if success
+int dequeueList(Node node); // removes the head of the linked list (Like in a queue structure), returns 0 if success
 
-int pullList(Node node);//removes the head of the linked list (Like in a stack structure), returns 0 if success
+int pushList(Node node); // Inserts a node as a new head of the Linked list (Like a stack structure), returns 0 if success
 
-int freeTree(BinaryTree* bt);//frees the tree from memory
+int pullList(Node node); // removes the head of the linked list (Like in a stack structure), returns 0 if success
 
-int freeList(LinkedList* ll);//frees the linked list from memory
+int freeTree(BinaryTree *bt); // frees the tree from memory
 
-int freeNode(Node* n);//frees node from memory
+int freeList(LinkedList *ll); // frees the linked list from memory
 
-int copyNode(Node* node, Node n);//copies a node's content into another one (would be like Node1 = Node2)
+int freeNode(Node *n); // frees node from memory
 
+int copyNode(Node *node, Node n); // copies a node's content into another one (would be like Node1 = Node2)
--- src/mainImplement.c
+++ src/mainImplement.c
@@ -1,61 +1,66 @@
 ﻿#include "mainHeader.h"
 #include <stdio.h>
 
-int newBT(BinaryTree* bt ,Node Root)//used to Initialise a tree, returns 0 if success
+int copytostring(String *s, String copyvalue)  // copies the value of one string to another string
 {
-    if(bt == NULL){return -1;}
+    s->length = copyvalue.length;
+    
+    for(int i = 0; i <= (*s).length; i++)
+    {
+
+    }
+}
+
+int newBT(BinaryTree *bt, Node Root) // used to Initialise a tree, returns 0 if success
+{
+    if (bt == NULL)
+    {
+        return -1;
+    }
     else
     {
-        bt->Lbranch = NULL;//No Left child
-        bt->Rbranch = NULL;//No right child
-        bt->Parent = NULL;//No parent
-        bt->Leaf.DataType = Root.DataType;
+        bt->Lbranch = NULL; // No Left child
+        bt->Rbranch = NULL; // No right child
+        bt->Parent = NULL;  // No parent
+        /*bt->Leaf.DataType = Root.DataType;*/ //Unsafe...
+        //TO DO STRINGSSSSSSSS
         bt->Leaf.Data_P = Root.Data_P;
     }
     return 0;
 }
 
-int newLl(LinkedList* ln, Node Head)//used to initialise a Linked List, returns 0 if success
+int newLl(LinkedList *ln, Node Head) // used to initialise a Linked List, returns 0 if success
 {
-
 }
 
-int addLeaf(Node Leaf, Node* Parent)//Add a leaf to a tree, given it's parents, returns 0 if success
+int addLeaf(Node Leaf, Node *Parent) // Add a leaf to a tree, given it's parents, returns 0 if success
 {
-
 }
 
-int queueList(Node node)//Appends a node to the linked list (like in a queue structure), returns 0 if success
+int queueList(Node node) // Appends a node to the linked list (like in a queue structure), returns 0 if success
 {
-
 }
 
-int dequeueList(Node node)//removes the head of the linked list (Like in a queue structure), returns 0 if success
+int dequeueList(Node node) // removes the head of the linked list (Like in a queue structure), returns 0 if success
 {
-
 }
 
-int pushList(Node node)//Inserts a node as a new head of the Linked list (Like a stack structure), returns 0 if success
+int pushList(Node node) // Inserts a node as a new head of the Linked list (Like a stack structure), returns 0 if success
 {
-
 }
 
-int pullList(Node node)//removes the head of the linked list (Like in a stack structure), returns 0 if success
+int pullList(Node node) // removes the head of the linked list (Like in a stack structure), returns 0 if success
 {
-
 }
 
-int freeTree(BinaryTree* bt)//frees the tree from memory
+int freeTree(BinaryTree *bt) // frees the tree from memory
 {
-
 }
 
-int freeList(LinkedList* ll)//frees the linked list from memory
+int freeList(LinkedList *ll) // frees the linked list from memory
 {
-
 }
 
-int freeNode(Node* n)//frees node from memory
+int freeNode(Node *n) // frees node from memory
 {
-
 }
